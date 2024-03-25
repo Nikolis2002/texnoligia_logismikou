bdef6e70a4a5daac39671cbc292638cf949bebcd
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-03-25 21:45:36 +0200

    Update MainActivity.java
    test

--- Code/app/src/main/java/com/ceid/texnolgia_logismikou/MainActivity.java
+++ Code/app/src/main/java/com/ceid/texnolgia_logismikou/MainActivity.java
@@ -44,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        System.out.println("ZZZZZZBILIOS");
     }
 
     @Override
