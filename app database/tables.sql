8b403c075db1041f15650f41c7e04b92f952caaf
Nikolis2002 <79275400+Nikolis2002@users.noreply.github.com>
2024-05-02 18:04:57 +0300

    fist version of sql tables

--- app database/tables.sql
+++ app database/tables.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS app_database
+DROP DATABASE IF EXISTS app_database;
 CREATE DATABASE app_database;
 USE app_database;
 
@@ -17,6 +17,8 @@ CREATE TABLE user
 CREATE TABLE customer
 (
     id INT UNSIGNED NOT NULL,
+    licence VARCHAR(32),
+    licence_image LONGBLOB,
     points INT UNSIGNED NOT NULL,
 
     CONSTRAINT fk_customer_id 
@@ -25,87 +27,185 @@ CREATE TABLE customer
         ON DELETE CASCADE
 );
 
-CREATE TABLE Taxi_driver
-(
-    id INT UNSIGNED NOT NULL,
-
-    CONSTRAINT fk_taxi_driver_id 
-        FOREIGN KEY(id) REFERENCES user(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-
-);
-
-
-CREATE TABLE Wallet
-(
-    id INT UNSIGNED NOT NULL,
-    user_id INT UNSIGNED NOT NULL,
-    balance DECIMAL(10, 2) NOT NULL,
-
-    CONSTRAINT userId
-        FOREIGN KEY(user_id) REFERENCES user(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-
-    PRIMARY KEY(id)
-);
-
-CREATE table Card
-(
-    wallet_id INT UNSIGNED NOT NULL,
-    card_number STRING NOT NULL,
-    card_holder STRING NOT NULL,
-    expiration_date STRING NOT NULL,
-    cvv STRING NOT NULL,
-    card_type STRING NOT NULL
-
-    CONSTRAINT walletId
-        FOREIGN KEY(wallet_id) REFERENCES Wallet(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-
-    PRIMARY KEY(wallet_id)
-);
-
-CREATE TABLE Coupon
-(
-    id STRING NOT NULL,
-    value DECIMAL(10, 2) NOT NULL,
-
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE Customer_Coupon
-(
-    coupon_id STRING NOT NULL,
-    customer_id INT UNSIGNED NOT NULL,
-    assignment_date DATETIME,
-
-    CONSTRAINT couponId
-        FOREIGN KEY(coupon_id) REFERENCES Coupon(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-
-    CONSTRAINT customerId
-        FOREIGN KEY(customer_id) REFERENCES customer(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-
-);
-
-CREATE TABLE Payment
+CREATE TABLE payment
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     customer_id INT UNSIGNED NOT NULL,
     payment_value DECIMAL(10, 2) NOT NULL,
-    payment_method ENUM('Wallet','Cash')
+    payment_method ENUM('WALLET','CASH'),
+
+    PRIMARY KEY(id),
 
     CONSTRAINT customersPaymentId
         FOREIGN KEY(customer_id) REFERENCES customer(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 
+);
+
+CREATE TABLE service
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    payment_id INT UNSIGNED,
+    service_status ENUM('ONGOING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'ONGOING',
+    status_date DATETIME, -- Cancellation or completion date. Originally NULL
+
+    PRIMARY KEY(id),
+
+    CONSTRAINT fk_service_payment
+    FOREIGN KEY(payment_id) REFERENCES payment(id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+);
+
+CREATE TABLE customer_history
+(
+    customer_id INT UNSIGNED NOT NULL,
+    service_id INT UNSIGNED NOT NULL,
+    
+    CONSTRAINT fk_customer_history_id 
+        FOREIGN KEY(customer_id) REFERENCES customer(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_service_id 
+        FOREIGN KEY(service_id) REFERENCES service(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+
+);
+
+CREATE TABLE transport
+(
+    licence_plate VARCHAR(32) NOT NULL,
+    manuf_date DATETIME NOT NULL,
+    coords POINT NOT NULL,
+
+    PRIMARY KEY(licence_plate)
+);
+
+CREATE TABLE taxi
+(
+    licence_plate VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY(licence_plate),
+
+    CONSTRAINT fk_taxi_transport
+    FOREIGN KEY(licence_plate) REFERENCES transport(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE rental
+(
+    licence_plate VARCHAR(32) NOT NULL,
+    free_status BOOLEAN NOT NULL,
+    rate DECIMAL(5, 2) NOT NULL,
+    model VARCHAR(32) NOT NULL DEFAULT 'FORD MONDEO',
+
+    CONSTRAINT fk_rental_transport
+    FOREIGN KEY(licence_plate) REFERENCES transport(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE car
+(
+    licence_plate VARCHAR(32) NOT NULL,
+    gas_level INT UNSIGNED NOT NULL,
+    seat_capacity INT UNSIGNED NOT NULL,
+
+    CONSTRAINT fk_car_rental
+    FOREIGN KEY(licence_plate) REFERENCES rental(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE motorcycle
+(
+    licence_plate VARCHAR(32) NOT NULL,
+    gas_level INT UNSIGNED NOT NULL,
+
+    CONSTRAINT fk_motorcycle_rental
+    FOREIGN KEY(licence_plate) REFERENCES rental(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE taxi_driver
+(
+    id INT UNSIGNED NOT NULL,
+    taxi VARCHAR(32) NOT NULL,
+
+    CONSTRAINT fk_taxi_driver_id 
+        FOREIGN KEY(id) REFERENCES user(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_taxi_driver_taxi
+    FOREIGN KEY(taxi) REFERENCES taxi(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE wallet
+(
+    id INT UNSIGNED NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL,
+
+    PRIMARY KEY(id),
+
+    CONSTRAINT fk_wallet_user
+        FOREIGN KEY(user_id) REFERENCES user(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE  
+);
+
+CREATE table card
+(
+    wallet_id INT UNSIGNED NOT NULL,
+    card_number VARCHAR(32) NOT NULL,
+    card_holder VARCHAR(32) NOT NULL,
+    expiration_date VARCHAR(32) NOT NULL,
+    cvv VARCHAR(32) NOT NULL,
+    card_type VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY(card_number),
+
+    CONSTRAINT fk_card_wallet
+        FOREIGN KEY(wallet_id) REFERENCES wallet(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE coupon
+(
+    id VARCHAR(32) NOT NULL,
+    value DECIMAL(10, 2) NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE customer_coupon
+(
+    coupon_id VARCHAR(32) NOT NULL,
+    customer_id INT UNSIGNED NOT NULL,
+    assignment_date DATETIME,
+
+    PRIMARY KEY(coupon_id, customer_id),
+
+    CONSTRAINT couponId
+        FOREIGN KEY(coupon_id) REFERENCES coupon(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT customerId
+        FOREIGN KEY(customer_id) REFERENCES customer(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE rental_rating
@@ -115,7 +215,7 @@ CREATE TABLE rental_rating
     comment TEXT,
 
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE taxi_rating
 (
@@ -125,7 +225,7 @@ CREATE TABLE taxi_rating
     comment TEXT,
 
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE out_city_rating
 (
@@ -135,69 +235,143 @@ CREATE TABLE out_city_rating
     comment TEXT,
 
     PRIMARY KEY(id)
-)
+);
 
-CREATE TABLE service
+CREATE TABLE gas_station
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    payment_id INT,
-    service_status ENUM('ONGOING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'ONGOING',
-    status_date DATETIME, -- Cancellation or completion date. Originally NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    coords POINT NOT NULL,
+    gas_price DECIMAL(5,2) NOT NULL,
 
     PRIMARY KEY(id)
+);
 
-    CONSTRAINT fk_service_payment
-    FOREIGN KEY(payment_id) REFERENCES payment(id)
-    ON UPDATE CASCADE,
-    ON DELETE SET NULL
-)
+CREATE TABLE refill
+(
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    station_id INT UNSIGNED NOT NULL,
+    initial_gas_quantity INT UNSIGNED NOT NULL,
+    added_gas_quantity INT UNSIGNED NOT NULL DEFAULT 0,
+    success BOOLEAN NOT NULL, -- If tracker fails after refill, mark this refill session as incomplete. Return money to customer at a later date
+
+    PRIMARY KEY(date),
+
+    CONSTRAINT fk_refill_gas_station
+    FOREIGN KEY(station_id) REFERENCES gas_station(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
 
 CREATE TABLE rental_service
 (
-    rating_id INT UNSIGNED NOT NULL,
-    refill_date DATETIME 
+    service_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    selected_vehicle VARCHAR(32) NOT NULL,
+    refill_date DATETIME, -- NULL, unless the customer refills the vehicle
+    rating_id INT UNSIGNED,
+    left_side_img LONGBLOB,
+    right_side_img LONGBLOB,
+    front_side_img LONGBLOB,
+    back_side_img LONGBLOB,
 
-    PRIMARY
+    PRIMARY KEY(service_id),
 
-    FOREIGN KEY(rating) REFERENCES rental_rating(id)
+    CONSTRAINT fk_rental_service
+    FOREIGN KEY(service_id) REFERENCES service(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_rental_vehicle
+    FOREIGN KEY(selected_vehicle) REFERENCES rental(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_rental_rating
+    FOREIGN KEY(rating_id) REFERENCES rental_rating(id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL,
+
+    CONSTRAINT fk_rental_refill
+    FOREIGN KEY(refill_date) REFERENCES refill(date)
     ON UPDATE CASCADE
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE out_city_service
 (
-    rating_id INT UNSIGNED NOT NULL,
+    service_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    rating_id INT UNSIGNED,
 
-    FOREIGN KEY(rating_id) REFERENCES rental_rating(id)
+    PRIMARY KEY(service_id),
+
+    CONSTRAINT fk_out_city_rating
+    FOREIGN KEY(rating_id) REFERENCES out_city_rating(id)
     ON UPDATE CASCADE
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE taxi_request
 (
-    service_id INT NOT NULL AUTO_INCREMENT,
+    service_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     pickup_location POINT NOT NULL,
     destination POINT NOT NULL,
     assigned_driver INT UNSIGNED,
     assignment_time DATETIME, -- Originally NULL, until a driver is assigned
     pickup_time DATETIME, -- Originally NULL, until the driver picks up the customer
-    rating_id INT UNSIGNED NOT NULL,
+    rating_id INT UNSIGNED,
 
-    PRIMARY KEY(service_id)
+    PRIMARY KEY(service_id),
 
     CONSTRAINT fk_taxi_request_service
     FOREIGN KEY(service_id) REFERENCES service(id)
-    ON UPDATE CASCADE,
-    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
 
     CONSTRAINT fk_taxi_request_driver
     FOREIGN KEY(assigned_driver) REFERENCES taxi_driver(id)
-    ON UPDATE CASCADE,
-    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
 
     CONSTRAINT fk_taxi_request_rating
     FOREIGN KEY(rating_id) REFERENCES taxi_rating(id)
-    ON UPDATE CASCADE,
-    ON DELETE CASCADE
-)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+);
+
+CREATE TABLE out_city_transport
+(
+    out_city_licence VARCHAR(32) NOT NULL,
+    seat_capacity INT UNSIGNED NOT NULL,
+    gas INT UNSIGNED NOT NULL,
+    
+    CONSTRAINT fk_licence_plate 
+    FOREIGN KEY(out_city_licence) REFERENCES transport(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    
+    PRIMARY KEY(out_city_licence)
+);
+
+CREATE TABLE out_city_car
+(
+    out_city_licence VARCHAR(32) NOT NULL,
+
+    CONSTRAINT fk_out_city_car_licence_plate 
+    FOREIGN KEY(out_city_licence) REFERENCES transport(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+    PRIMARY KEY(out_city_licence)
+);
+
+CREATE TABLE out_city_van
+(
+    out_city_licence VARCHAR(32) NOT NULL,
+
+    CONSTRAINT fk_out_city_van_licence_plate 
+    FOREIGN KEY(out_city_licence) REFERENCES transport(licence_plate)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+    PRIMARY KEY(out_city_licence)
+);
+
