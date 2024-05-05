46f6abdff13b618ac2b1cb2fc19e73b40ff85a3a
PKalozoumis <panagiotis.kalozoumis@gmail.com>
2024-05-05 19:19:21 +0300

    Started InCity

--- Code/app/src/main/java/com/ceid/ui/InCityVehicleScreen.java
+++ Code/app/src/main/java/com/ceid/ui/InCityVehicleScreen.java
@@ -2,6 +2,7 @@ package com.ceid.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InCityVehicleScreen extends AppCompatActivity
 {
@@ -10,5 +11,12 @@ public class InCityVehicleScreen extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.in_city_vehicle_screen);
+
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+
+        TextView textview = findViewById(R.id.in_city_choose_vehicle);
+
+		textview.setText(String.format("%s %s", textview.getText(), extras.getString("type")));
     }
 }
