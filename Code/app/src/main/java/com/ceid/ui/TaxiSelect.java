2c1b136aac12cae75e4fcf4908f5f3f33cf71e48
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-06 17:12:00 +0300

    Update TaxiSelect.java

--- Code/app/src/main/java/com/ceid/ui/TaxiSelect.java
+++ Code/app/src/main/java/com/ceid/ui/TaxiSelect.java
@@ -1,4 +1,20 @@
 package com.ceid.ui;
 
-public class TaxiSelect {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaxiSelect extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.taxi_select);
+
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+
+        
+    }
+
 }
