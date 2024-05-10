6d52a8f9d58dd5a8425ea72635a33be0a961a488
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-10 22:47:31 +0300

    Create Location.java

---
+++ Code/app/src/main/java/com/ceid/util/Location.java
@@ -0,0 +1,16 @@
+package com.ceid.util;
+
+public class Location extends Coordinates{
+
+    private final String Address;
+
+    public Location(double lat, double lng, String AddressName) {
+        super(lat, lng);
+        this.Address = AddressName;
+    }
+
+    public String getAddress(){
+        return Address;
+    }
+
+}
