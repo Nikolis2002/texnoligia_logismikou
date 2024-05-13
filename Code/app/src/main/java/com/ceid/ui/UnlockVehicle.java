0d26d9f58f8a57b60c91a6fb439f0a75a3aa5f7c
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-13 22:06:28 +0300

    Create Unlock vehicle

---
+++ Code/app/src/main/java/com/ceid/ui/UnlockVehicle.java
@@ -0,0 +1,53 @@
+package com.ceid.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ceid.util.Coordinates;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class UnlockVehicle extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private Coordinates car_loc;
+    private int car_id;
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unlock_screen);
+        mapSetup();
+
+        Intent data = getIntent();
+        car_id = data.getIntExtra("car_id",-1);
+        car_loc = (Coordinates) data.getSerializableExtra("car_location");
+
+    }
+
+    private void mapSetup(){
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.googleMap,mapFragment).commit();
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map=googleMap;
+
+        LatLng location = new LatLng(car_loc.getLat(),car_loc.getLng());
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
+
+        Marker carMarker = map.addMarker(new MarkerOptions()
+                .position(location));
+    }
+}
