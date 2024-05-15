d841b7a8548b60dcdfc21d14dc746b7d1e9f759a
PKalozoumis <panagiotis.kalozoumis@gmail.com>
2024-05-15 10:47:25 +0300

    .

--- Code/app/src/main/java/com/ceid/ui/MainScreen.java
+++ Code/app/src/main/java/com/ceid/ui/MainScreen.java
@@ -1,5 +1,16 @@
 package com.ceid.ui;
 
-public class MainScreen
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainScreen extends AppCompatActivity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+
 }
