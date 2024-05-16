23f161df23779cfbc9ecf0d279e9468de4a5c7b8
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-16 17:01:03 +0300

    Taxi waiting Screen

---
+++ Code/app/src/main/java/com/ceid/ui/TaxiWaitScreen.java
@@ -0,0 +1,15 @@
+package com.ceid.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaxiWaitScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.taxi_request_wait_screen);
+
+    }
+}
