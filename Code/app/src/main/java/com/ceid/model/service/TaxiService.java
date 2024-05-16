0d618592af0ad2829523c37ccc6aea8f0c12ff53
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-16 15:58:42 +0300

    Taxi Classes creation

---
+++ Code/app/src/main/java/com/ceid/model/service/TaxiService.java
@@ -0,0 +1,13 @@
+package com.ceid.model.service;
+
+import com.ceid.model.payment_methods.Payment;
+import com.ceid.model.transport.Transport;
+
+import java.time.LocalDateTime;
+
+public class TaxiService extends Service{
+
+    public TaxiService(int id, LocalDateTime creationDate, Payment payment, Rating rating, Transport transport) {
+        super(id, creationDate, payment, rating, transport);
+    }
+}
