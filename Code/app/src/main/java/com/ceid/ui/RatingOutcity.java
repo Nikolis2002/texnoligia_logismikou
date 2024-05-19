3a238c60bc32113273ede86f207e5baf6ccd4e47
PKalozoumis <panagiotis.kalozoumis@gmail.com>
2024-05-19 19:41:25 +0300

    Rating

---
+++ Code/app/src/main/java/com/ceid/ui/RatingOutcity.java
@@ -0,0 +1,19 @@
+package com.ceid.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class RatingOutcity extends Fragment
+{
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.rating_outcity, container, false);
+	}
+}
+
---
+++ Code/app/src/main/java/com/ceid/ui/RatingTaxi.java
@@ -0,0 +1,21 @@
+package com.ceid.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class RatingTaxi extends Fragment
+{
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+		return inflater.inflate(R.layout.rating_taxi, container, false);
+	}
+}
+
