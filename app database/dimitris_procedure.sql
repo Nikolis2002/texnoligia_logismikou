a05dec6570e5fa863d10daf37cc2f3921ae477a5
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-05-24 16:15:55 +0300

    Update dimitris_procedure.sql

--- app database/dimitris_procedure.sql
+++ app database/dimitris_procedure.sql
@@ -85,7 +85,7 @@ drop procedure if exists checkTaxiRequest;
 delimiter $
 create procedure checkTaxiRequest(in request_id int)
 begin
-	declare service_st int;
+	declare service_st  VARCHAR(32);
 	declare serviceId int;
 	declare taxi_req__driver VARCHAR(32);
 
@@ -93,7 +93,7 @@ begin
 	select service_status into service_st from service where id=serviceId;
 	select assigned_driver into taxi_req__driver from taxi_request where id=request_id;
 	
-	if service_status="ONGOING" AND assigned_driver IS NULL THEN
+	if service_status='ONGOING' AND assigned_driver IS NULL THEN
 		SELECT "TRUE" AS result;
 	ELSE
 		SELECT "FALSE" AS result; 
