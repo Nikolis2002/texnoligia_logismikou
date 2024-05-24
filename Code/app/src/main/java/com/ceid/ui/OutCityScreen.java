c108c6dc65dc2e2115bb01c2fb9a687d2247d774
PKalozoumis <panagiotis.kalozoumis@gmail.com>
2024-05-24 18:18:31 +0300

    .

--- Code/app/src/main/java/com/ceid/ui/OutCityScreen.java
+++ Code/app/src/main/java/com/ceid/ui/OutCityScreen.java
@@ -3,6 +3,7 @@ package com.ceid.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -15,22 +16,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ceid.Network.ApiClient;
+import com.ceid.Network.ApiService;
 import com.ceid.model.transport.Garage;
 import com.ceid.model.transport.OutCityCar;
 import com.ceid.model.transport.OutCityTransport;
 import com.ceid.model.transport.Rental;
 import com.ceid.model.transport.Van;
 import com.ceid.util.Coordinates;
+import com.ceid.util.GenericCallback;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OutCityScreen extends AppCompatActivity implements AdapterView.OnItemClickListener, ActivityResultCallback<ActivityResult> {
 
     private RecyclerView recyclerView;
     private GarageListAdapterOld adapter;
-    private ArrayList<Garage> garageList;
     private Bundle locationScreenData = null;
     private Intent locationIntent;
     private ActivityResultLauncher<Intent> activityResultLauncher;
@@ -59,15 +72,86 @@ public class OutCityScreen extends AppCompatActivity implements AdapterView.OnIt
         activityResultLauncher.launch(locationIntent);
     }
 
-    private ArrayList<Garage> retrieveGarages()
+    private ArrayList<Garage> retrieveGarages(GenericCallback<ArrayList<Garage>> callback)
     {
-        //Communicate with database
-        //...
-        //...
-        //...
-        //Communication complete
+        ApiService api = ApiClient.getApiService();
 
-        garageList = new ArrayList<>();
+        Call<ResponseBody> call = api.getGarages();
+
+        call.enqueue(new Callback<ResponseBody> (){
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response)
+            {
+                if (response.isSuccessful())
+                {
+                    ArrayList<Garage> garageList = new ArrayList<>();
+                    String jsonString = null;
+
+                    try
+                    {
+                        jsonString = response.body().string();
+                    }
+                    catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+
+                    ArrayNode garageListData;
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    try
+                    {
+                        garageListData = (ArrayNode)mapper.readTree(jsonString);
+                    }
+                    catch (JsonProcessingException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+
+                    for (JsonNode garageData : garageListData)
+                    {
+                        ArrayList<OutCityTransport> vehicleList = new ArrayList<>();
+
+                        for (JsonNode vehicleData : (ArrayNode)garageListData.get("vehicles"))
+                        {
+                            if (vehicleData.get("type").asText() == "car")
+                            {
+                                /*
+                                vehicleList.add(new OutCityCar(
+                                        vehicleData.get("license_plate").asText(),
+                                        vehicleData.get("rate").asDouble(),
+                                        vehicleData.get("seats").asInt(),
+                                        vehicleData.get("license_plate").asDouble(),
+                                        vehicleData.get("license_plate").asDouble(),
+                                        vehicleData.get("license_plate").asDouble()
+                                ));*/
+                            }
+                        }
+
+
+                        /*
+                        garageList.add(new Garage(
+
+                        ))*/
+                    }
+
+                    callback.onSuccess(garageList);
+                }
+                else
+                {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable throwable)
+            {
+
+            }
+        });
+
+        ArrayList<Garage> garageList = new ArrayList<>();
 
         ArrayList<OutCityTransport> l = new ArrayList<>();
         l.add(new OutCityCar("1", 20, 1, 1,"dista", "1", "2024"));
@@ -194,7 +278,20 @@ public class OutCityScreen extends AppCompatActivity implements AdapterView.OnIt
                 text.setText(String.format("%s %s", getResources().getString(R.string.location),selectedCoords.toString()));
 
                 //Get garages
-                this.garageList = retrieveGarages();
+                ArrayList<Garage> garageList = retrieveGarages(new GenericCallback<ArrayList<Garage>> (){
+
+                    @Override
+                    public void onSuccess(ArrayList<Garage> data)
+                    {
+
+                    }
+
+                    @Override
+                    public void onFailure(Exception e)
+                    {
+
+                    }
+                });
 
                 if (garageList.isEmpty())
                 {
@@ -212,20 +309,15 @@ public class OutCityScreen extends AppCompatActivity implements AdapterView.OnIt
         }
     }
 
-    /*
-    private void addGarage(String garage){
-        garageList.add(garage);
-        adapter.notifyItemInserted(garageList.size()-1);
-    }
-    */
-
     //Clicking on list item
     public void onItemClick(AdapterView<?> parent, View clickedItem, int position, long id)
     {
-        String test=garageList.get(position).getName();
+        Garage garage = (Garage)clickedItem.getTag();
+
+        String test=garage.getName();
         Intent intent=new Intent(OutCityScreen.this, GarageInfoScreen.class);
         Bundle bundle = new Bundle();
-        bundle.putSerializable("garage", garageList.get(position));
+        bundle.putSerializable("garage", garage);
         intent.putExtras(bundle);
         startActivity(intent);
     }
